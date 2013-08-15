a3bdffdfee0eba81ea23e3f309b8017b3f6bfcb1
amoncsek <amo.ahcp@googlemail.com>
2013-08-15 16:15:06 +0200

    add demos and maven archetypes for JacpFX

---
+++ JacpFX-simple-archetype/target/classes/archetype-resources/src/main/java/callbacks/StatefulCallback.java
@@ -0,0 +1,31 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.callbacks;
+
+import java.util.logging.Logger;
+
+import javafx.event.Event;
+
+import org.jacp.api.action.IAction;
+import org.jacp.javafx2.rcp.component.ACallbackComponent;
+
+/**
+ * A StatefulCallback component is a simple background component whose result
+ * will be always send back to the caller component. Unlike a Steteless
+ * Component you can use it like a "normal" bean.
+ * 
+ * @author Andy Moncsek
+ * 
+ */
+public class StatefulCallback extends ACallbackComponent {
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+	@Override
+	public Object handleAction(IAction<Event, Object> arg0) {
+		// runs in worker thread
+		logger.info(arg0.getLastMessage().toString());
+		return "StatefulCallback - hello";
+	}
+
+}
---
+++ JacpFX-simple-archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/components/ComponentLeft.java
@@ -0,0 +1,82 @@
+package it.pkg.components;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import org.jacp.api.action.IAction;
+import org.jacp.api.action.IActionListener;
+import org.jacp.javafx2.rcp.component.AFX2Component;
+import org.jacp.javafx2.rcp.componentLayout.FX2ComponentLayout;
+import org.jacp.javafx2.rcp.util.FX2Util.MessageUtil;
+
+/**
+ * a simple UI component, creates a ScrollPane with a Button and a label. The Button will trigger a message to the StatefulCallback component.
+ * 
+ * @author Andy Moncsek
+ * 
+ */
+public class ComponentLeft extends AFX2Component {
+	private ScrollPane pane;
+	private Label leftLabel;
+
+	@Override
+	public Node handleAction(IAction<Event, Object> action) {
+		// runs in worker thread
+		if (action.getLastMessage().equals(MessageUtil.INIT)) {
+			return createInitialLayout();
+		}
+		return null;
+	}
+
+	@Override
+	public Node postHandleAction(Node arg0, IAction<Event, Object> action) {
+		// runs in FX application thread
+		if (action.getLastMessage().equals(MessageUtil.INIT)) {
+			this.pane = (ScrollPane) arg0;
+		} else {
+			leftLabel.setText(action.getLastMessage().toString());
+		}
+		return this.pane;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ScrollPane createInitialLayout() {
+		final ScrollPane pane = new ScrollPane();
+		pane.setFitToHeight(true);
+		pane.setFitToWidth(true);
+		GridPane.setHgrow(pane, Priority.ALWAYS);
+		GridPane.setVgrow(pane, Priority.ALWAYS);
+		final VBox box = new VBox();
+		final Button left = new Button("Left");
+		leftLabel  = new Label("");
+		// create message to StatefulCallback component 
+		IActionListener<EventHandler<Event>, Event, Object> listener = getActionListener("id01.id003", "hello stateful component");
+		left.setOnMouseClicked((EventHandler<? super MouseEvent>) listener);
+		VBox.setMargin(left, new Insets(4, 2, 4, 5));
+		box.getChildren().addAll(left,leftLabel);
+		pane.setContent(box);
+		return pane;
+	}
+
+	@Override
+	public void onStartComponent(FX2ComponentLayout arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTearDownComponent(FX2ComponentLayout arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
