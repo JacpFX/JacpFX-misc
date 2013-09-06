4548dbe7eea714ab401d9ac0a5e192c79b5fbdaf
AmoAHCP <amo.ahcp@googlemail.com>
2013-09-06 08:39:34 +0200

    add test packages

---
+++ JacpFX-TestCases/src/test/java/org/jacp/test/workbench/LoadWorkbenchTest.java
@@ -0,0 +1,11 @@
+package org.jacp.test.workbench;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ady
+ * Date: 06.09.13
+ * Time: 08:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoadWorkbenchTest {
+}
