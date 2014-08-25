238c311a88bf1360cee07faf3806711b22eaab47
amoAHCP <amo.ahcp@gmail.com>
2014-08-25 15:24:35 +0200

    deleted MQTT Demo
    MQTT is now included in remote-draw demo

--- JacpFX-remote-draw-mqtt/src/main/java/org/jacpfx/dto/ClearMessage.java
+++
@@ -1,9 +0,0 @@
-package org.jacpfx.dto;
-
-import java.io.Serializable;
-
-/**
- * Created by Andy Moncsek on 17.12.13.
- */
-public class ClearMessage implements Serializable {
-}
--- JacpFX-remote-draw-mqtt/src/main/java/org/jacpfx/dto/FragmentNavigation.java
+++
@@ -1,8 +0,0 @@
-package org.jacpfx.dto;
-
-/**
- * Created by amo on 06.02.14.
- */
-public enum FragmentNavigation {
-    BACK, CONNECT, CREATE, FINISH
-}
