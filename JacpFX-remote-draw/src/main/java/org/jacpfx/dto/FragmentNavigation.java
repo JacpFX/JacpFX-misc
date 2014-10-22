70557a21dc5c1da0b0101409913bd50a30ffd353
amoAHCP <amo.ahcp@gmail.com>
2014-10-22 22:39:32 +0200

    moved JacpFX-remote draw to JacpFX-demos

--- JacpFX-remote-draw/src/main/java/org/jacpfx/dto/FragmentNavigation.java
+++
@@ -1,8 +0,0 @@
-package org.jacpfx.dto;
-
-/**
- * Created by amo on 06.02.14.
- */
-public enum FragmentNavigation {
-    SHOW_VERTX, BACK_VERTX, CONNECT_VERTX, CREATE_VERTX, FINISH_VERTX, SHOW_MQTT, BACK_MQTT, CONNECT_MQTT, CREATE_MQTT, FINISH_MQTT
-}
